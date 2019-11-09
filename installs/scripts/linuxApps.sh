9606ab44034c55d9bdda4b4d118b35a3d3660b16
kensleDev <julianiaquinandi@gmail.com>
2019-11-09 18:35:24 +0000

    :hammer: added chrome install

--- installs/scripts/linuxApps.sh
+++ installs/scripts/linuxApps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install software-properties-common 
+sudo apt-get install software-properties-common
 
 sudo apt-get install xbindkeys
 sudo apt-get install xvkbd
@@ -18,6 +18,17 @@ sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/v
 sudo apt update
 sudo apt install -y code
 
+# Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm -rf google-chrome-stable_current_amd64.deb
 
+# Dark theme
+mkdir tmp && cd tmp
+git clone https://github.com/surajmandalcell/elementary-x.git  ~/.themes/elementary-x
+git clone https://github.com/keeferrourke/la-capitaine-icon-theme.git  ~/.icons/la-capitaine-icon-theme
+gsettings set org.gnome.desktop.interface gtk-theme 'elementary-x'
+gsettings set org.gnome.desktop.interface icon-theme 'la-capitaine-icon-theme'
+gsettings set org.pantheon.desktop.gala.appearance button-layout 'close,minimize,maximize'
 
 sudo snap install postman
