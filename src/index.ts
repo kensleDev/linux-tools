a3e32b1338b25a063321891820d64e245c955b4d
kensleDev <kensledev@gmail.com>
2019-11-28 21:41:00 +0000

    :fireworks: started ts implementation

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+console.log('Try npm run check/fix!');
+
+const longString =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
+
+const trailing = 'Semicolon';
+
+const why = 'am I tabbed?';
+
+export function doSomeStuff(
+  withThis: string,
+  andThat: string,
+  andThose: string[]
+) {
+  console.log(withThis);
+  console.log(andThat);
+  console.dir(andThose);
+  //function on one line
+  if (!andThose.length) {
+    return false;
+  } return undefined
+
+}
+// TODO: more examples
