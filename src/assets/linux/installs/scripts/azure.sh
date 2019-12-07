3e1f89f49e3130b95ae0e211240bd5c04f533898
kensleDev <kensledev@gmail.com>
2019-12-07 12:34:05 +0000

    Update azure.sh

--- src/assets/linux/installs/scripts/azure.sh
+++ src/assets/linux/installs/scripts/azure.sh
@@ -6,10 +6,10 @@ wget http://mirrors.kernel.org/ubuntu/pool/main/i/icu/libicu60_60.2-6ubuntu1_amd
 sudo dpkg -i libicu60_60.2-6ubuntu1_amd64.deb
 
 sudo apt-get update
-sudo apt-get install dotnet-sdk-2.1
+sudo apt-get install dotnet-sdk-2.1 -y
 
 
 wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
-sudo apt-get install azure-functions-core-tools
+sudo apt-get install azure-functions-core-tools -y
